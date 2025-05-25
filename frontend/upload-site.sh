42b29cdb20ead317958f801e4adba40b6b1d24ef
Wannes Persyn <wannes.persyn@gmail.com>
2025-05-25 15:45:08 +0200

    Fix Routing Bug V7

--- frontend/upload-site.sh
+++ frontend/upload-site.sh
@@ -23,7 +23,7 @@ find "$local_folder" -type f | while read -r file_path; do
         echo "Uploading $relative_path..."
 
         # Construct the Blob Storage URL for the file
-        blob_url="https://$storage_account.blob.core.windows.net/$container_name/$relative_path"
+        blob_url="https://$storage_account.blob.core.windows.net/$container_name/$relative_path?$sas_token"
         blob_url=$(echo "$blob_url" | tr -d ' ')
 
         # Set Content-Type based on file extension
